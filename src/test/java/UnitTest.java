fbb1e743db75052c80027f3e7ffa0d8b53b83d9e
JMcQueen1000 <jmcqueen1000@gmail.com>
2023-10-11 17:54:25 +1100

    Added unit tests

---
+++ src/test/java/UnitTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UnitTest {
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void UnitTest1() {
+        int i = 1;
+        assertEquals(1, i);
+    }
+
+    @Test
+    void UnitTest2() {
+        int i = 2;
+        assertEquals(2, i);
+    }
+
+    @Test
+    void UnitTest3() {
+        int i = 3;
+        assertEquals(3, i);
+    }
+}
